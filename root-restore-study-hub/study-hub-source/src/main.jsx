8eb3e9b75b0b09620aebaec84774f5814e9c6a03
NerdyGirlJas <jasmyneharp@yahoo.com>
2026-08-28 15:25:29 -0400

    Delete root-restore-study-hub/study-hub-source directory

--- root-restore-study-hub/study-hub-source/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import StudyHub from './StudyHub.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <StudyHub />
-  </React.StrictMode>
-);
